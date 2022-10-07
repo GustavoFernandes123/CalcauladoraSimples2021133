f46a5b8fcb942ce9936d338e0228fc8be23ba78f
PabloMaximoP <pmaximopeixoto@gmail.com>
2022-10-07 16:19:46 -0300

    classe para conta

--- CalcauladoraSimples2021133/CalculosSimples.cs
+++ CalcauladoraSimples2021133/CalculosSimples.cs
@@ -12,5 +12,24 @@ public class CalculosSimples
         return soma;
     }
 
+    public double Subtrair(double numero1, double numero2)
+    {
+        double subtracao = numero1 - numero2;
+        return subtracao;
+    }
+
+    public double Dividir(double numero1, double numero2)
+    {
+        double divisao = numero1 / numero2;
+        return divisao;
+    }
     
+    public double Multiplicar(double numero1, double numero2)
+    {
+        double multiplicacao = numero1 * numero2;
+        return multiplicacao;
+    }
+
+
+
 }
